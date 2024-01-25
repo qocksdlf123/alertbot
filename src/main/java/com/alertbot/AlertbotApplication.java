9815f5a8c452d5ad65242c98c88bd8a753e67bf7
qocksdlf123 <qocksdlf123@naver.com>
2024-01-25 23:56:38 +0900

    Feat) alertbot 프로젝트 시작

---
+++ src/main/java/com/alertbot/AlertbotApplication.java
@@ -0,0 +1,13 @@
+package com.alertbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alertbot/AlertbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alertbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
